a3f29e95729a8070bc87a1dd5fcbda1e0b9206bb
Rajat <rajatmudhol123@gmail.com>
2025-12-15 03:00:53 +0530

    redix url fix

--- src/config/redix.ts
+++ src/config/redix.ts
@@ -1,9 +1,15 @@
 import IORedis from "ioredis";
+import dotenv from "dotenv";
 
-export const redis = new IORedis(process.env.REDIS_URL!, {
+dotenv.config();
+
+if (!process.env.REDIS_URL) {
+  throw new Error("REDIS_URL is not set in environment variables");
+}
+
+export const redis = new IORedis(process.env.REDIS_URL, {
   maxRetriesPerRequest: null,
+  enableOfflineQueue: true,
 });
-
-
 
 export default redis;
